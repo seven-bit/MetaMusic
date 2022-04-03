3b14238773e2ff5c6b90ef3e4ffde99e6e99a644
Ankit singh <ak94307@gmail.com>
2022-04-03 21:03:22 +0530

    Added link to github repo for details

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hey there!\n The server is working';
+    // return 'Hey there!\n The server is working';
+    return 'https://github.com/seven-bit/MetaMusic';
   }
 }
